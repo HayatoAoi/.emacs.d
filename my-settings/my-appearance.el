1792bc125ff65d09873421d795b9b559c7dadc6c
HayatoAoi <aoi.8810.hyt@gmail.com>
2020-04-15 16:11:12 +0900

    Moving my-local-settings

--- my-settings/my-appearance.el
+++ my-settings/my-appearance.el
@@ -16,11 +16,5 @@
 ;;  nil 'japanese-jisx0208
 ;;  (font-spec :family "Ricty"))
 
-(when (equal system-name "muga2")
-  (set-face-attribute 'default nil
-                   :family "Ricty"
-                   :height 130)
-      )
-
 
 (provide 'my-appearance)
---
+++ my-settings/my-local-settings.el
@@ -0,0 +1,45 @@
+;;; my-local-settings.el --- My local settings       -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  
+
+;; Author:  <hayato@optiplex>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
+(when (equal system-name "muga2")
+  (set-face-attribute 'default nil
+                   :family "Ricty"
+                   :height 130)
+      )
+
+
+(when (equal system-name "optiplex")
+  (load-theme 'tsdh-dark t)
+      )
+
+
+;; (load-theme 'misterioso t)
+;; (load-theme 'adwaita t)
+
+
+(provide 'my-local-settings)
+;;; my-local-settings.el ends here
