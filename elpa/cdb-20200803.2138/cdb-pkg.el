bd7b73c6cb431d4a3be27498873b9594c182eb1d
HayatoAoi <aoi.8810.hyt@gmail.com>
2025-03-04 02:32:20 +0900

    upgrade for Emacs 30.1

--- elpa/cdb-20200803.2138/cdb-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/hayato/.emacs.d/elpa/cdb-20200803.2138/cdb.el  -*- no-byte-compile: t -*-
-(define-package "cdb" "20200803.2138" "constant database (cdb) reader for Emacs Lisp" 'nil :commit "81410b925afd4cfd1ca5ab6c1257e221e1cc664f" :keywords '("cdb") :authors '(("Yusuke Shinyama <yusuke at cs . nyu . edu>")) :maintainer '("SKK Development Team" . "skk@ring.gr.jp"))
